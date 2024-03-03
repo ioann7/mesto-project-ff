168281df52cd1b16db2a89bf0c42ddec70dff712
Ioann <ioann.chimrov@gmail.com>
2024-03-03 13:25:09 +0300

    add createCard and delete

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,31 @@
-// @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template');
+const placesList = document.querySelector('.places__list');
 
-// @todo: DOM узлы
 
-// @todo: Функция создания карточки
+const createCard = (data, deleteCallback) => {
+  const card = cardTemplate.content.cloneNode(true);
+  const cardTitle = card.querySelector('.card__title');
+  const cardImage = card.querySelector('.card__image');
+  const cardDeleteButton = card.querySelector('.card__delete-button');
 
-// @todo: Функция удаления карточки
+  cardTitle.textContent = data['name'];
+  cardImage.src = data['link'];
+  cardDeleteButton.addEventListener('click', deleteCallback);
 
-// @todo: Вывести карточки на страницу
+  return card;
+}
+
+
+const deleteCardCallback = event => {
+  card = event.target.closest('.places__item');
+  card.remove();
+}
+
+const renderCards = cards => {
+  for (const cardData of cards) {
+    placesList.append(createCard(cardData, deleteCardCallback));
+  }
+}
+
+
+renderCards(initialCards);
