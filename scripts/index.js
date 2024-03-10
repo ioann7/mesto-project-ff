c6fd323041fbc7f167e727203ac9d4260973f901
Ioann <ioann.chimrov@gmail.com>
2024-03-10 22:59:08 +0300

    правки после обзора кода

--- scripts/index.js
+++ scripts/index.js
@@ -4,27 +4,30 @@ const placesList = document.querySelector('.places__list');
 
 const createCard = (data, deleteCallback) => {
   const card = cardTemplate.content.cloneNode(true);
+  const cardElement = card.querySelector('.places__item');
   const cardTitle = card.querySelector('.card__title');
   const cardImage = card.querySelector('.card__image');
   const cardDeleteButton = card.querySelector('.card__delete-button');
 
   cardTitle.textContent = data['name'];
   cardImage.src = data['link'];
-  cardDeleteButton.addEventListener('click', deleteCallback);
+  cardImage.alt = data['name'];
+
+  cardDeleteButton.addEventListener('click', () => deleteCallback(cardElement));
 
   return card;
 }
 
 
-const deleteCardCallback = event => {
-  card = event.target.closest('.places__item');
+const deleteCardCallback = card => {
   card.remove();
 }
 
+
 const renderCards = cards => {
-  for (const cardData of cards) {
+  cards.forEach( cardData => {
     placesList.append(createCard(cardData, deleteCardCallback));
-  }
+  });
 }
 
 
