6523a4bad6c6740cf6075b1ea99b43b48ae1f1dc
ioann7 <ioann.chimrov@gmail.com>
2024-04-28 12:45:42 +0300

    add webpack

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+import './pages/index.css';
+import { initialCards } from './components/cards.js'
+
 const cardTemplate = document.querySelector('#card-template');
 const placesList = document.querySelector('.places__list');
 
